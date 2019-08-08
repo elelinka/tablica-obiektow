c58a27dd8fd5c7f2c076e7cd503cc0b954dd334b
Ewelina Gregoraszczuk <gregoraszczuk.e@gmail.com>
2019-08-08 11:36:32 +0200

    method has been changed

--- src/com/example/controller/DeviceController.java
+++ src/com/example/controller/DeviceController.java
@@ -7,47 +7,45 @@ import java.util.Scanner;
 
 public class DeviceController {
 
-    public static void getInputUser(Scanner input) {
+    public static void addObjectToArray(Scanner input) {
         Electronics[] electronics = new Electronics[3];
+
+        Electronics previous;
+        Electronics current;
+
+        String name;
+        int size;
+        String description;
 
         System.out.println("Wprowadź 3 urządzenia");
 
         for (int i = 0; i < electronics.length; i++) {
             System.out.println("nazwa");
-            String name = input.nextLine();
+            name = input.nextLine();
             System.out.println("rozmiar");
-            int size = input.nextInt();
-            System.out.println("opis");
+            size = input.nextInt();
             input.nextLine();
-            String description = input.nextLine();
-            electronics[i] = getElectronicsArray(name, size, description)[i];
+            System.out.println("opis");
+            description = input.nextLine();
+            electronics[i] = new Electronics(name, size, description);
+            previous = electronics[i];
+            for (int j = i + 1; j < electronics.length; j++) {
+                System.out.println("nazwa");
+                name = input.nextLine();
+                System.out.println("rozmiar");
+                size = input.nextInt();
+                input.nextLine();
+                System.out.println("opis");
+                description = input.nextLine();
+                electronics[j] = new Electronics(name, size, description);
+                current = electronics[j];
+                if (current.equals(previous)) {
+                    System.out.println("podaj inne urządzenie");
+                    j--;
+                    continue;
+                }
+            }
         }
-
         System.out.println(Arrays.asList(electronics));
     }
-
-    private static Electronics[] getElectronicsArray(String name, int size, String description) {
-        Electronics[] electronics = new Electronics[3];
-        Electronics current = null;
-        Electronics previous = null;
-
-
-            do {
-                for (int i = 0; i < electronics.length; i++) {
-                    electronics[i] = new Electronics(name, size, description);
-                    previous = current;
-                    current = electronics[i];
-                }
-            } while (!current.equals(previous));
-//            electronics[i] = new Electronics(name, size, description);
-//            previous = current;
-//            current = electronics[i];
-//            if (current.equals(previous)) {
-//                //System.out.println("wprowadź inne urządzenie");
-//                electronics[i - 1] = electronics[i];
-//            }
-
-        return electronics;
-    }
-
 }
